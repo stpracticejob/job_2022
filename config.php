8c3920972e540c0b513a2edcb054a968ec3b9297
NaikaMur <anitaclark112@gmail.com>
2022-05-07 22:14:55 +0500

    NaikaMur added API users v4.0

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-    $DSN = $_ENV['DSN'] ?? 'mysql:host=localhost;dbname=db_job;charset=utf8;';
+    $DSN = $_ENV['DSN'] ?? 'mysql:host=localhost;dbname=job;charset=utf8;';
     $DB_USER = $_ENV['DB_USER'] ?? 'root';
     $DB_PASSWORD = $_ENV['DB_PASSWORD'] ?? '';
     $DB_OPTIONS = $_ENV['DB_OPTIONS'] ?? [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'];
