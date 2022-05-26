7fde849d6900b4c945f6c451df53c6122ea59a27
Vendigo74 <tumypjkee225500@gmail.com>
2022-05-26 16:33:01 +0500

    Added bootstrap header on main page for anauthorized users

--- views/main_page.php
+++ views/main_page.php
@@ -3,20 +3,21 @@
 	<head>
 		<title></title>
 		<link rel="stylesheet" href="/css/style.css"/>
-                <meta charset="utf-8" /> 
+		<meta charset="utf-8" /> 
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 	</head>
 	<body>
 		<?include('user_menu.inc');?>
-		<?include('header.inc');?>
-				<h1>Реклама</h1>
-				
-				<?foreach ($db->fetchAdvertises() as $item):?>
-				
-					<h2><?=$item['Title'] ?></h2>
-					<?=$item['Content'] ?>
-					<hr/>
-				<?endforeach;?>
-				</table>
+			<h1>Реклама</h1>
+			
+			<?foreach ($db->fetchAdvertises() as $item):?>
+			
+				<h2><?=$item['Title'] ?></h2>
+				<?=$item['Content'] ?>
+				<hr/>
+			<?endforeach;?>
+			</table>
 		<?include('footer.inc');?>	
 	</body>
 </html>
