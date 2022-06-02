2fe3460b5a192173779f6194d5079c1c6487d8f1
Vendigo74 <tumypjkee225500@gmail.com>
2022-06-02 23:12:35 +0500

    Added header to user directory and login page

--- views/auth_user.php
+++ views/auth_user.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html>
-	<head></head>
+	<head>
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css"/>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+	</head>
 	<body>
+	<?include("$_SERVER[DOCUMENT_ROOT]/../views/user_menu.inc");?>
 		<h1>Вход в учётную запись</h1>
 		<form action="/login" method="POST">
 			<b>Логин:</b><br/>
--- views/users/index.php
+++ views/users/index.php
@@ -2,13 +2,11 @@
 <html>
 	<head>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-		<script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
-		<script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>		
-		<link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.1/dist/jquery.validate.min.js"></script>
-		<script type="text/javascript" src="/scripts/jquery-validation/src/additional/pattern.js"></script>
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css"/>
+		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4-4.6.0/dt-1.12.1/datatables.min.css"/>
+		<script type="text/javascript" src="https://cdn.datatables.net/v/bs4-4.6.0/dt-1.12.1/datatables.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+
 		
 		<script type="text/javascript">
 			$(function() {
@@ -30,14 +28,14 @@
                             data: 'ID',
                             render: function(data, type) {
                                 return '<button type="button" name="update" id="'
-                                    + data + '" class="btn btn-warning btn-xs update">Редактировать</button>';
+                                    + data + '" class="btn btn-warning btn-sm update">Редактировать</button>';
                             }
                         },
                         {
                             data: 'ID',
                             render: function(data, type) {
                                 return '<button type="button" name="delete" id="'
-                                    + data + '" class="btn btn-danger btn-xs delete">Удалить</button>';
+                                    + data + '" class="btn btn-danger btn-sm delete">Удалить</button>';
                             }
                         },
                     ],
@@ -213,6 +211,7 @@
 		</script>
 	</head>
 	<body>
+		<?include("$_SERVER[DOCUMENT_ROOT]/../views/user_menu.inc");?>
 		<div class="container box">
 			<div class="table-responsive">
 				<br />
@@ -240,8 +239,8 @@
 				<form method="post" id="user_form" enctype="multipart/form-data">
 					<div class="modal-content">
 						<div class="modal-header">
-							<button type="button" class="close" data-dismiss="modal">&times;</button>
 							<h4 class="modal-title">Добавить пользователя</h4>
+							<button type="button" class="close" data-dismiss="modal">&times;</button>
 						</div>
 						<div class="modal-body">
 							<div class="field">
