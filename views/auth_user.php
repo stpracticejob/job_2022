f21b299999a892958f5e453d933f483af430aafc
Vendigo74 <tumypjkee225500@gmail.com>
2022-06-03 12:55:43 +0500

    Added include in auth_user

--- views/auth_user.php
+++ views/auth_user.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css"/>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+		<?include("head.inc");?>
 	</head>
 	<body>
 	<?include('user_menu.inc');?>
