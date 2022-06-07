a61e7a4dbc80fd299ff573da271fcce1686c246b
PalchunLev <92323037+PalchunLev@users.noreply.github.com>
2022-06-07 22:13:06 +0500

    latest 3 vacancies on main page (#35)
    * latest vacancies on mai paige v1
    
    * latest 3 vacancies on main page v1
    
    * latest 3 vacancies on main page v2
    
    * latest 3 vacancies on main page 3
    
    * latest 3 vacancies on main page v4
    
    * исправил размещение

--- views/main_page.php
+++ views/main_page.php
@@ -42,6 +42,32 @@
                     </div>
                     <?endforeach;?>
                 </div>
+
+                <h1 class="text-center">Последние Вакансии</h1>
+				<div class="row">
+                    <?php $Vacancies = $db->fetchVacancies(false, 3)->fetchAll(); ?>
+                    <?foreach ($Vacancies as $item):?>
+                        <div class="col-md-4 col-lg-4 col-sm-12">
+                        <div class="card">
+                            <div class="card-header">
+                                Категория: <?=$item['SectionName'] ?>
+                            </div>
+                            <div class="card-body">
+                                <h4 class="card-title"><?=$item['Title'] ?></h4>
+                                <p class="card-text">
+                                    <h2>Зарплата: <?=$item['Salary'] ?> </h2>
+                                    <hr/>
+									Дата публикации: <?=$item['DateTime'] ?>
+                                </p>
+                            </div>
+                            <div class="card-footer">
+                                <button type="button" class="btn btn-primary" data-toggle="modal"
+                                    data-target="#modalVacancy<?= $item['ID'] ?>">Подробности</button>
+                            </div>
+                        </div>
+                    </div>
+                    <?endforeach;?>
+                </div>
             </div>
         </div>
         <?foreach ($cvs as $item):?>
@@ -59,6 +85,32 @@
                         Должность: <?= $item['Title'] ?><br>
                         Описание: <?= $item['Content'] ?><br>
                         Пользователь: <?= $item['UserName'] ?><br>
+                        Дата публикации вакансии: <?= $item['DateTime'] ?>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
+                    </div>
+                </div>
+            </div>
+		</div>
+        <?endforeach;?>
+
+        <?foreach ($Vacancies as $item):?>
+        <div class="modal fade" id="modalVacancy<?= $item['ID'] ?>" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Вакансия</h4>
+                        <button type="button" class="close" data-dismiss="modal">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        Категория: <?= $item['SectionName'] ?><br>
+                        Должность: <?= $item['Title'] ?><br>
+                        Описание: <?= $item['Content'] ?><br>
+                        Зарплата: <?= $item['Salary'] ?><br>
+                        Требуемый опыт работы: <?= $item['Experience'] ?><br><hr/>
                         Дата публикации вакансии: <?= $item['DateTime'] ?>
                     </div>
                     <div class="modal-footer">
