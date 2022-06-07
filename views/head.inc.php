0c4c2b9e21a8a3133c0a25a29683a47eeab15869
Sergey Belinsky <sergeybe@gmail.com>
2022-06-08 02:26:48 +0500

    Fix position of footer by height of page

--- views/head.inc.php
+++ views/head.inc.php
@@ -1,3 +1,4 @@
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css"/>
+<link rel="stylesheet" type="text/css" href="/css/styles.css"/>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
