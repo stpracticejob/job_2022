2b68ad5ef03e978b7f5f78cf2088ad53cd398d55
Maximov Alex <madmgn99@yandex.ru>
2022-06-08 12:02:54 +0500

    Add 3 last adver with fix

--- views/main_page.php
+++ views/main_page.php
@@ -6,75 +6,9 @@
 		<? include("head.inc.php") ?>
 	</head>
 	<body>
-<<<<<<< HEAD
-		<?include('user_menu.inc');?>
-			
-        <?php $advertises = $db->fetchAdvertises(false, 3)->fetchAll(); ?>
-        <div>
-			<div class="container">
-			    <h1 class="text-center">Последняя Реклама</h1>
-				<div class="row">
-                    <?foreach ($advertises as $item):?>
-                    <div class="col-md-4 col-lg-4 col-sm-12">
-                        <div class="card">
-                            <div class="card-header">
-                                Реклама
-                            </div>
-                            <div class="card-body">
-                                <h4 class="card-title"><?=$item['Title'] ?></h4>
-                                <p class="card-text">
-                                    <?=$item['Content'] ?>
-                                </p>
-                            </div>
-                            <div class="card-footer">
-                                <button type="button" class="btn btn-primary" data-toggle="modal"
-                                    data-target="#modalAdvertises<?= $item['ID'] ?>">Подробности</button>
-                            </div>
-                        </div>
-                    </div>
-                    <?endforeach;?>
-                </div>
-            </div>
-        </div>
-
-        <?foreach ($advertises as $item):?>
-        <div class="modal fade" id="modalAdvertises<?= $item['ID'] ?>" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title">Реклама</h4>
-                        <button type="button" class="close" data-dismiss="modal">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        Заголовок: <?= $item['Title'] ?><br>
-                        Описание: <?= $item['Content'] ?><br>
-                        Дата публикации вакансии: <?= $item['DateTime'] ?>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
-                    </div>
-                </div>
-            </div>
-		</div>
-        <?endforeach;?>
-
-        <?php $cvs = $db->fetchCvs(false, 3)->fetchAll(); ?>
-=======
 		<? include('user_menu.inc.php') ?>
-        <div class="container">
-            <h1>Реклама</h1>
-            <? foreach ($db->fetchAdvertises() as $item): ?>
-
-                <h2><?=$item['Title'] ?></h2>
-                <?=$item['Content'] ?>
-                <hr/>
-            <? endforeach ?>
-        </div>
 
         <? $cvs = $db->fetchCvs(false, 3)->fetchAll(); ?>
->>>>>>> master
         <div>
 			<div class="container">
 			    <h1 class="text-center">Последние Резюме</h1>
@@ -100,6 +34,27 @@
                     <? endforeach ?>
                 </div>
 
+                <?php $advertises = $db->fetchAdvertises(false, 3)->fetchAll(); ?>
+        <div>
+			<div class="container">
+			    <h1 class="text-center">Последняя Реклама</h1>
+				<div class="row">
+                    <?foreach ($advertises as $item):?>
+                    <div class="col-md-4 col-lg-4 col-sm-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title"><?=$item['Title'] ?></h4>
+                                <p class="card-text">
+                                    <?=$item['Content'] ?>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <?endforeach;?>
+                </div>
+            </div>
+        </div> 
+        
                 <h1 class="text-center">Последние Вакансии</h1>
 				<div class="row">
                     <? $vacancies = $db->fetchVacancies(false, 3)->fetchAll() ?>
